40272192bd830a29b61965ce526b6ada0f91fbce
Janek <eunho.moon@gmail.com>
2023-07-06 11:02:11 +0900

    docker-compose

--- docker-compose-app/server.js
+++ docker-compose-app/server.js
@@ -9,11 +9,18 @@ const app = express();
 
 client.set('number', 0);
 
-app.get('/', (req, res) => {
-  client('number', (err, number) => {
-    client.set('number', Number(number) + 1);
-    res.send(`숫자가 1씩 올라갑니다. 숫자: ${number}`);
-  });
+app.get('/', async (req, res) => {
+  await client.connect();
+  let number = await client.get('number');
+
+  if (number === null) number = 0;
+
+  console.log('Number: ' + number);
+
+  res.send(`숫자가 1씩 올라갑니다. 숫자: ${number}`);
+
+  await client.set('number', parseInt(number) + 1);
+  await client.disconnect();
 });
 
 app.listen(8080);
